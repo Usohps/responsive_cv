c38ce6ad6675a3146f046a539f8e3b36ab6d779c
Princewill Samuel Usoh <85501301+Usohps@users.noreply.github.com>
2022-02-03 07:26:42 -0800

    Add files via upload

---
+++ Mycv/test.js
@@ -0,0 +1,16 @@
+ 
+// function checkname(){
+//   var name = document.getElementsByClassName("name").value;
+//   if(name.length > 15){
+//     return alert("Too many Characters at $name");
+//   }else{
+//      console.log("OK")
+//   }
+// }
+// console.log(name);
+
+
+
+
+
+
